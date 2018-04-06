4afed64d99d79cdd2a4419fa0c6dee42f6a2bfe8
Luomin1993 <879671933@qq.com>
2018-04-06 17:31:16 +0800

    add map of Nodes

--- Methods/computeF.cpp
+++ Methods/computeF.cpp
@@ -7,11 +7,11 @@ using namespace std;
 class compute_symbol
 {
 public:
-	compute_symbol();
-	~compute_symbol();
-	void printFormula();
-	string giveFormula();
-	void setParas();
+    compute_symbol();
+    ~compute_symbol();
+    void printFormula();
+    string giveFormula();
+    void setParas();
 public:
     string symbol;
     int    para_num;
@@ -23,19 +23,19 @@ class sqrt_symbol:compute_symbol
 public:
     sqrt_symbol()
     {
-    	symbol = "√";
-    	paras.push_back("x");
-    	para_num = paras.size();
+        symbol = "√";
+        paras.push_back("x");
+        para_num = paras.size();
     }
 
     void printFormula()
     {
-    	cout<<giveFormula()<<endl;
-    }	
+        cout<<giveFormula()<<endl;
+    }    
 
     string giveFormula()
     {
-    	return "√(" + paras[0] + ")";
+        return "√(" + paras[0] + ")";
     }
 };
 
@@ -47,20 +47,20 @@ class integral:compute_symbol
 public:
     integral()
     {
-    	symbol = "∲d";
-    	paras.push_back("f(x)");
-    	paras.push_back("x");
-    	para_num = paras.size();
+        symbol = "∲d";
+        paras.push_back("f(x)");
+        paras.push_back("x");
+        para_num = paras.size();
     }
 
     void printFormula()
     {
-    	cout<<giveFormula()<<endl;
-    }	
+        cout<<giveFormula()<<endl;
+    }    
 
     string giveFormula()
     {
-    	return "∲(" + paras[0] + ")" + "d" + paras[1];
+        return "∲(" + paras[0] + ")" + "d" + paras[1];
     }
 };
 
@@ -68,11 +68,11 @@ public:
 
 struct symbol_node
 {
-	compute_symbol cs;
-	int paras_num;
-	bool is_leaf;
-	string leaf_symbol;
-	vector<symbol_node*> sons;
+    compute_symbol cs;
+    int paras_num;
+    bool is_leaf;
+    string leaf_symbol;
+    vector<symbol_node*> sons;
 };
 
 
@@ -83,13 +83,13 @@ struct symbol_node
 class method
 {
 public:
-	method(){};
-	//~method();
-	//string giveResult();
+    method(){};
+    //~method();
+    //string giveResult();
 public:
-	string (*giveResult)(vector<string> paras);
-	vector<string> paras;
-	vector<string> results;	
+    string (*giveResult)(vector<string> paras);
+    vector<string> paras;
+    vector<string> results;    
 };
 
 
@@ -98,71 +98,122 @@ public:
 
 string Energy(vector<string> paras)
 {
-	if (paras.size()==0)
-	{
-		return "mv²/2+U"; 
-	}
-	return paras[0] + paras[1] + "²/2" + "+" + paras[2];
+    if (paras.size()==0)
+    {
+        return "mv²/2+U"; 
+    }
+    return paras[0] + paras[1] + "²/2" + "+" + paras[2];
 }
 
 string Jene(vector<string> paras)
 {
-	if (paras.size()==0)
-	{
-		return "∲√(2m(E-U))dx";
-	}
-	return "∲" + paras[0] + "d" + paras[1];
+    if (paras.size()==0)
+    {
+        return "∲√(2m(E-U))dx";
+    }
+    return "∲" + paras[0] + "d" + paras[1];
 }
 
 string Derivation(vector<string> paras)
 {
-	if (paras.size()==0)
-	{
-		return "∂F/∂x";
-	}
-	return "∂(" + paras[0] + ")/∂(" + paras[1] +")";
+    if (paras.size()==0)
+    {
+        return "∂F/∂x";
+    }
+    if (paras.size()==1)
+    {
+        return "∂(" + paras[0] + ")/∂x";
+    }
+    return "∂(" + paras[0] + ")/∂(" + paras[1] +")";
 }
 
 string inverse(vector<string> paras)
 {
-	if (paras.size()==0)
-	{
-		return "1/x";
-	}
-	return "1/(" + paras[0] + ")"; 	
+    if (paras.size()==0)
+    {
+        return "1/x";
+    }
+    return "1/(" + paras[0] + ")";     
 }
 
 method broker(method m1,method m2,string relationship)
 {
-	method res;
-	if( relationship=="parallel")
-	{
-	    res.results.push_back(m1.giveResult(vector<string>() ));
-	    res.results.push_back(m2.giveResult(vector<string>() ));
-	    return res;
-	}
-	if( relationship=="serialize")
-	{
-	    m2.paras = m1.results;
-	    res.results.push_back(m2.giveResult(m2.paras));
-	    return res;
-	}
-	//case "recurrent":
-	return res;
+    method res;
+    if( relationship=="parallel")
+    {
+        res.results.push_back(m1.giveResult(vector<string>() ));
+        res.results.push_back(m2.giveResult(vector<string>() ));
+        return res;
+    }
+    if( relationship=="serialize")
+    {
+        m2.paras = m1.results;
+        res.results.push_back(m2.giveResult(m2.paras));
+        return res;
+    }
+    //case "recurrent":
+    return res;
 }
 
+method broker(method m,string relationship)
+{
+	//method res;
+	if (relationship=="recurrent")
+	{
+		if (m.paras.size()==0)
+		{
+			m.paras.push_back(m.giveResult(m.paras));
+			return m;
+		}
+		m.paras[0] = m.giveResult(m.paras);
+		//m.results.push_back(m.giveResult(m.paras));
+		return m;
+	}
+	return m;
+}
 
+void split(const string& s, vector<string>& v, const string& c)
+{
+    string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while(string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2-pos1));
+         
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
 
 
 
 int main(int argc, char const *argv[])
 {
-	/* code */
-	method E;E.giveResult = Energy;
-	method J;J.giveResult = Jene;
-	method D;D.giveResult = Derivation;
-	method iv;iv.giveResult = inverse;
-	//cout<<broker(E,J,"parallel").results[0]<<endl;
-	cout<<"f = "<<broker(broker(broker(E,J,"parallel"),D,"serialize"),iv,"serialize").results[0]<<endl;
-	return 0;
+    /* code */
+    method E;E.giveResult = Energy;
+    method J;J.giveResult = Jene;
+    method D;D.giveResult = Derivation;
+    method iv;iv.giveResult = inverse;
+    //cout<<broker(E,J,"parallel").results[0]<<endl;
+    map<string,method> MethodsMap;
+    MethodsMap["energy"] = E;
+    MethodsMap["jene"]   = J;
+    MethodsMap["derivation"] = D;
+    MethodsMap["inverse"]    = iv;
+
+    string text = "We let the energy and the jene to do the derivation,and it's inverse is the frequence;";
+    vector<string> a;
+    split(text,a," ");
+    for(int i=0;i<a.size();i++)
+    {cout<<a[i]<<endl;}
+    
+
+    cout<<" "<<endl;
+    cout<<"f = "<<broker(broker(broker(E,J,"parallel"),D,"serialize"),iv,"serialize").results[0]<<endl;
+
+    cout<<"f = "<<broker(broker(D,"recurrent"),"recurrent").paras[0]<<endl;
+    return 0;
 }
--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,11 @@
+/*
+Created on Wed Apr 04 22:02:19 2018
+
+@author: Hanss401
+
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -87,6 +95,26 @@ Node* sym(string exp)
 	//sym->expression = exp;
 }
 
+Node* num(string exp)
+{
+	//static Node* num = new Node;
+	static std::vector<Node*> num_statck;
+	num_statck.push_back(new Node);
+	/*if (num->expression!="")
+	{
+		cout<<"WTF"<<endl;
+	}*/
+	if (num_statck.size()<MAX_USE_SYM)
+	{
+		int last = num_statck.size()-1;
+		num_statck[last]->expression = exp;
+		num_statck[last]->type       = "Num";
+		return num_statck[last];
+	}
+	return NULL;
+	//num->expression = exp;
+}
+
 Node* add(Node* s1,Node* s2)
 {
 	static std::vector<Node*> add_stack;
@@ -109,6 +137,227 @@ Node* add(Node* s1,Node* s2)
 }
 
 
+Node* times(Node* s1,Node* s2)
+{
+	static std::vector<Node*> times_stack;
+	times_stack.push_back(new Node);
+	if (times_stack.size()<MAX_USE_SYM)
+	{
+		int last = times_stack.size()-1;
+		times_stack[last]->SubNodes.push_back(s1);
+		times_stack[last]->SubNodes.push_back(s2);
+		times_stack[last]->expression = "("+s1->expression+"×"+s2->expression+")";
+		return times_stack[last];
+	}
+	return NULL;
+}
+
+Node* equal(Node* s1,Node* s2)
+{
+	static std::vector<Node*> equal_stack;
+	equal_stack.push_back(new Node);
+	if (equal_stack.size()<MAX_USE_SYM)
+	{
+		int last = equal_stack.size()-1;
+		equal_stack[last]->SubNodes.push_back(s1);
+		equal_stack[last]->SubNodes.push_back(s2);
+		equal_stack[last]->expression = s1->expression+" = "+s2->expression;
+		return equal_stack[last];
+	}
+	return NULL;
+}
+
+
+
+
+//---------- Example of Pm Equation --------
+Node* derivation(Node* s1,Node* s2)
+{
+	static std::vector<Node*> derivation_stack;
+	derivation_stack.push_back(new Node);
+	if (derivation_stack.size()<MAX_USE_SYM)
+	{
+		int last = derivation_stack.size()-1;
+		derivation_stack[last]->SubNodes.push_back(s1);
+		derivation_stack[last]->SubNodes.push_back(s2);
+		derivation_stack[last]->expression = "∂"+s1->expression+"/∂"+s2->expression;
+		return derivation_stack[last];
+	}
+	return NULL;
+}
+
+
+
+Node* sum(Node* s1)
+{
+	static std::vector<Node*> sum_stack;
+	sum_stack.push_back(new Node);
+	if (sum_stack.size()<MAX_USE_SYM)
+	{
+		int last = sum_stack.size()-1;
+		sum_stack[last]->SubNodes.push_back(s1);
+		//sum_stack[last]->SubNodes.push_back(s2);
+		sum_stack[last]->expression = "∑"+s1->expression;
+		return sum_stack[last];
+	}
+	return NULL;
+}
+
+
+Node* integral(Node* s1,Node* s2)
+{
+	static std::vector<Node*> integral_stack;
+	integral_stack.push_back(new Node);
+	if (integral_stack.size()<MAX_USE_SYM)
+	{
+		int last = integral_stack.size()-1;
+		integral_stack[last]->SubNodes.push_back(s1);
+		integral_stack[last]->SubNodes.push_back(s2);
+		integral_stack[last]->expression = "∫"+s1->expression+"d"+s2->expression;
+		return integral_stack[last];
+	}
+	return NULL;
+}
+
+
+
+Node* ln(Node* s1)
+{
+	static std::vector<Node*> ln_stack;
+	ln_stack.push_back(new Node);
+	if (ln_stack.size()<MAX_USE_SYM)
+	{
+		int last = ln_stack.size()-1;
+		ln_stack[last]->SubNodes.push_back(s1);
+		//ln_stack[last]->SubNodes.push_back(s2);
+		ln_stack[last]->expression = "ln("+s1->expression+")";
+		return ln_stack[last];
+	}
+	return NULL;
+}
+
+
+Node* exp(Node* s1)
+{
+	static std::vector<Node*> exp_stack;
+	exp_stack.push_back(new Node);
+	if (exp_stack.size()<MAX_USE_SYM)
+	{
+		int last = exp_stack.size()-1;
+		exp_stack[last]->SubNodes.push_back(s1);
+		//exp_stack[last]->SubNodes.push_back(s2);
+		exp_stack[last]->expression = "exp("+s1->expression+")";
+		return exp_stack[last];
+	}
+	return NULL;
+}
+
+Node* der(Node* s1)
+{
+	static std::vector<Node*> der_stack;
+	der_stack.push_back(new Node);
+	if (der_stack.size()<MAX_USE_SYM)
+	{
+		int last = der_stack.size()-1;
+		der_stack[last]->SubNodes.push_back(s1);
+		//der_stack[last]->SubNodes.push_back(s2);
+		der_stack[last]->expression = "d"+s1->expression;
+		return der_stack[last];
+	}
+	return NULL;
+}
+
+Node* sub(Node* s1,Node* s2)
+{
+	static std::vector<Node*> sub_stack;
+	sub_stack.push_back(new Node);
+	if (sub_stack.size()<MAX_USE_SYM)
+	{
+		int last = sub_stack.size()-1;
+		sub_stack[last]->SubNodes.push_back(s1);
+		sub_stack[last]->SubNodes.push_back(s2);
+		sub_stack[last]->expression = s1->expression+"-"+s2->expression;
+		return sub_stack[last];
+	}
+	return NULL;
+}
+
+Node* devide(Node* s1,Node* s2)
+{
+	static std::vector<Node*> devide_stack;
+	devide_stack.push_back(new Node);
+	if (devide_stack.size()<MAX_USE_SYM)
+	{
+		int last = devide_stack.size()-1;
+		devide_stack[last]->SubNodes.push_back(s1);
+		devide_stack[last]->SubNodes.push_back(s2);
+		devide_stack[last]->expression = s1->expression+"/"+s2->expression;
+		return devide_stack[last];
+	}
+	return NULL;
+}
+
+
+//mapStudent.insert(map<int, string>::value_type (1, "student_one"));
+//namespace method
+static std::map<Node*,Node*> Methods;
+
+void signin(std::map<Node*,Node*>& Methods)
+{
+	//std::map<Node*,Node*> Methods;
+
+	// move item
+	static Node* move_item_key   = equal(sym("a") , sub(sym("b"),sym("c")));
+	static Node* move_item_value = equal(add(sym("a"),sym("c")) , sym("b"));
+	//Methods.insert(map<Node*,Node*>::value_type (move_item_key,move_item_value));
+	Methods[move_item_key]       = move_item_value;
+	Methods[move_item_value]     = move_item_key;
+
+	// times devide
+	static Node* times_devide_key   = equal(sym("a") , devide(sym("b"),sym("c")));
+	static Node* times_devide_value = equal(times(sym("a"),sym("c"))  ,sym("b"));
+	Methods[times_devide_key]       = times_devide_value;
+	Methods[times_devide_value]     = times_devide_key;
+
+	// int de
+	static Node* int_de_key   = integral(sym("f")  ,sym("x"));
+	static Node* int_de_value = derivation(sym("f"),sym("x"));
+	Methods[int_de_key]       = int_de_value;
+	Methods[int_de_value]     = int_de_key;
+
+	// int
+	static Node* int_by_sides_A_key   = equal(times(sym("f"),der(sym("x"))), der(sym("t")));
+	static Node* int_by_sides_A_value = equal(integral(sym("f"),der(sym("x"))), sym("t"));
+	Methods[int_by_sides_A_key]       = int_by_sides_A_value;
+
+	static Node* special_int_key      = equal(integral(devide(num("1"),sub(sym("S"),times(sym("a"),sym("N")))),sym("N")) ,
+		                                                                                     integral(num("1"),sym("t")) );
+	static Node* special_int_value    = equal(add(times(devide(num("-1"),sym("a")),ln(sub(sym("S"),times(sym("a"),sym("N"))))),sym("C")), sym("t"));
+	Methods[special_int_key]          = special_int_value;
+
+
+	// 
+
+}
+
+
+void TEST_expression_int()
+{
+	signin(Methods);
+	cout<<(--Methods.end())->first->expression<<endl;
+	cout<<(--Methods.end())->second->expression<<endl;
+	//cout<<special_int_value->expression<<endl;
+}
+
+//static Node* expression_test = equal(derivation(sym("N"),sym("t")) , sym("N\'") );
+
+void TEST_expression()
+{
+	static Node* expression_test = equal(derivation(sym("N"),sym("t")) , sym("N\'") );
+	cout<<expression_test->expression<<endl;
+}
+
+
 
 
 int main(int argc, char const *argv[])
@@ -118,6 +367,9 @@ int main(int argc, char const *argv[])
 	//Node a = Sym("A");Node b = Sym("B");
 	//cout<<Add(a,b).show()<<endl;
 	//cout<<Sym("B").show()<<endl;
-	cout<<add(sym("a"),add(sym("b"),sym("c")) )->expression<<endl;
+	
+	//cout<<add(sym("a"),add(sym("b"),sym("c")) )->expression<<endl;
+	//cout<<expression_test->expression<<endl;
+	TEST_expression_int();
 	return 0;
 }
